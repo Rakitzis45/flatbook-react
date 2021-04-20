1417c4dbc9c58a213038102041b3810db1705f02
Rakitzis45 <michael.rakitzis@gmail.com>
2021-04-20 12:05:26 -0400

    create CreateUser component

---
+++ src/containers/users/CreateUser.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import {connect} from 'react-redux'
+
+class CreateUser extends Component {
+    state = { 
+        user: "",
+        password: ""
+    }
+   handleChange = event => {
+       this.setState({
+           [event.target.name]: event.target.value
+       })
+   }
+    render(){
+        return (
+            <div>
+                <form>
+                    <label>Email:</label>
+                    <input type="text" value={this.state.user} onChange={this.handleChange} name='user'/>
+                    <br/>
+                    <label> Password: </label>
+                    <input type="password" value={this.state.password} onChange={this.handleChange} name='password'/>
+                    <input type="submit"/>
+                </form> 
+            </div>
+        )
+    }
+}
+
+function mapDispatchToProps(dispatch){
+    return {
+        loginUser: (user, password) => dispatch(loginUser(user, password))
+    }
+}
+
+export default connect(null, mapDispatchToProps)(CreateUser)
