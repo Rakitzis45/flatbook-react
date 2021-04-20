a966a624ddf6e75e0a04b8fdd36d43790183a726
Rakitzis45 <michael.rakitzis@gmail.com>
2021-04-20 11:05:49 -0400

    create reducer

---
+++ src/reducer/reducer.js
@@ -0,0 +1,23 @@
+export default function reducer(state = { currentUser: {} }, action) {
+    switch (action.type) {
+        case "START_ADDING_USER_REQUEST":
+            return {
+                ...state,
+                user: {},
+                requesting: true
+            }
+        case "ADD_USER":
+            return {
+                ...state,
+                user: action.user,
+                requesting: false
+            }
+        case "REMOVE_USER":
+            return {
+                ...state,
+                user: {}
+            }
+        default: 
+            return state;
+    }
+}
