282552a4ad9d5467e2a0d42c7e9624b452fb2505
Rakitzis45 <michael.rakitzis@gmail.com>
2021-04-21 11:55:18 -0400

    import UserShow

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import CreateUser from './containers/users/CreateUser'
+import UserShow from './containers/users/UserShow'
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Route exact path="/" component={CreateUser}/>
+        <Route exact path="/users/:id" component={UserShow}/>
       </div>
     </Router>
     
