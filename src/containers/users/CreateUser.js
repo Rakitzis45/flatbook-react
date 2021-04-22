4cd2ec58b53ea33da2f4f9115c5793704195e349
Rakitzis45 <michael.rakitzis@gmail.com>
2021-04-22 12:49:54 -0400

    import action and withRouter, created event listener handleSubmit

--- src/containers/users/CreateUser.js
+++ src/containers/users/CreateUser.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux'
+import {loginUser} from '/home/rakitzis45/flatbook/flatbook-react/src/actions/user.js'
+import { withRouter } from 'react-router-dom';
 
 class CreateUser extends Component {
     state = { 
@@ -11,10 +13,15 @@ class CreateUser extends Component {
            [event.target.name]: event.target.value
        })
    }
+   handleSubmit = event => {
+       event.preventDefault()
+       let user = {email: this.state.user, password: this.state.password}
+       this.props.loginUser(user, this.props.history)
+   }
     render(){
         return (
             <div>
-                <form>
+                <form onSubmit={this.handleSubmit}>
                     <label>Email:</label>
                     <input type="text" value={this.state.user} onChange={this.handleChange} name='user'/>
                     <br/>
@@ -29,8 +36,8 @@ class CreateUser extends Component {
 
 function mapDispatchToProps(dispatch){
     return {
-        loginUser: (user, password, history) => dispatch(loginUser(user, password, history))
+        loginUser: (user, history) => dispatch(loginUser(user, history))
     }
 }
 
-export default connect(null, mapDispatchToProps)(CreateUser)
+export default withRouter(connect(null, mapDispatchToProps)(CreateUser))
