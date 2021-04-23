0d83bddd5e74eed458bfef306b5107c7dd403c33
turpp <white.tripp@icloud.com>
2021-04-23 10:07:02 -0500

    added signup to routes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import LoginUser from './containers/users/LoginUser'
 import UserShow from './containers/users/UserShow'
+import SignupUser from './containers/users/SignupUser'
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <div className="App">
         <Route exact path="/" component={LoginUser}/>
         <Route exact path="/users/:id" component={UserShow}/>
+        <Route exact path='/signup' component={SignupUser}/>
       </div>
     </Router>
     
