27c22f860db552f0f70d516a954af51f669379a8
turpp <white.tripp@icloud.com>
2021-04-23 12:32:49 -0500

    added signup action

--- src/actions/user.js
+++ src/actions/user.js
@@ -20,3 +20,34 @@ export const loginUser = (user, history) => {
             })
     }
 }
+
+export const signupUser = (user, history)=>{
+    return(dispatch)=>{
+        dispatch({ type: "START_ADDING_USER_REQUEST" })
+        const url = "http://localhost:3000/signup"
+        fetch(url, {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            credentials: 'include',
+            body: JSON.stringify({
+                user: user
+            })
+        })
+            .then(resp => resp.json())
+            .then(user => {
+                // debugger
+                dispatch({ type: "ADD_USER", payload: { user: user } })
+                history.push(`/users/${user.id}`)
+            })
+    }
+
+
+
+    }
+
+
+
+
