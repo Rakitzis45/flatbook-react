35e8ea97ae66d7cf403ca0404870f299ce324201
turpp <white.tripp@icloud.com>
2021-04-25 16:51:05 -0500

    temp navbar with logout button to test features

---
+++ src/containers/NavBar.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {connect} from 'react-redux'
+import {withRouter} from 'react-router-dom'
+import {logout} from '../actions/user'
+class NavBar extends React.Component {
+
+    handleClick = (event)=>{
+        this.props.logout(this.props.history)
+    }
+
+    render() {
+        return <div><button onClick={this.handleClick}>Logout</button></div>
+    }
+}
+
+const mapDispatchToProps = (dispatch)=>{
+    return {
+        logout: (history)=> dispatch(logout(history))
+    }
+}
+
+export default withRouter(connect(null, mapDispatchToProps)(NavBar))
